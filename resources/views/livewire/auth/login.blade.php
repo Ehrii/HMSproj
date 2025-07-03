3705f123509554e41969775770cd1532a52c1aab
Michael <michaeljohn.labton1@gmail.com>
2025-07-03 11:10:19 +0800

    Tuloy mo login jez

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,24 +1,25 @@
 <div class="flex flex-col gap-6">
-    <x-auth-header :title="__('Log in to your account')" :description="__('Enter your email and password below to log in')" />
 
     <!-- Session Status -->
     <x-auth-session-status class="text-center" :status="session('status')" />
 
     <form wire:submit="login" class="flex flex-col gap-6">
         <!-- Email Address -->
-        <flux:input
+        <x-mary-input
             wire:model="email"
             :label="__('Email address')"
             type="email"
             required
             autofocus
             autocomplete="email"
-            placeholder="email@example.com"
+            placeholder="Enter Username"
+            clearable
+            icon="o-user"
         />
 
         <!-- Password -->
         <div class="relative">
-            <flux:input
+            <x-mary-password
                 wire:model="password"
                 :label="__('Password')"
                 type="password"
@@ -29,9 +30,9 @@
             />
 
             @if (Route::has('password.request'))
-                <flux:link class="absolute end-0 top-0 text-sm" :href="route('password.request')" wire:navigate>
+                <a class="absolute end-0 top-0 text-sm" href="route('password.request')" wire:navigate>
                     {{ __('Forgot your password?') }}
-                </flux:link>
+                </a>
             @endif
         </div>
 
