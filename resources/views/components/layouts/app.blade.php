368a800bcf5edb8ccb4e18b33859888ebe025702
ehrii <cjohneriel@gmail.com>
2025-07-04 15:54:57 +0800

    Initial Interface

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -11,7 +11,7 @@
 <body class="font-sans antialiased">
 
     {{-- The navbar with `sticky` and `full-width` --}}
-    <x-mary-nav sticky full-width>
+    <x-mary-nav sticky full-width class="bg-gradient-to-r from-[#ef4444] to-[#ed1c24] shadow-lg">
 
         <x-slot:brand>
             {{-- Drawer toggle for "main-drawer" --}}
@@ -20,14 +20,41 @@
             </label>
 
             {{-- Brand --}}
-            <div>Health Information System</div>
+            <div class="avatar">
+                <div class="w-12 p rounded-full shadow-lg mr-3">
+                    <label class="swap swap-rotate">
+                        <input type="checkbox" />
+                        <div class="swap-on">
+                            <img src="images/taguig-logo.svg" alt="On State" />
+                        </div>
+                        <div class="swap-off">
+                            <img src="images/taguig-logo.svg" alt="Off State" />
+                        </div>
+                    </label>
+                </div>
+            </div>
+            <div class="text-white font-bold text-2xl">City Hall Office</div>
         </x-slot:brand>
 
         {{-- Right side actions --}}
-        <x-slot:actions>
-            <x-mary-button label="Messages" icon="o-envelope" link="###" class="btn-ghost btn-sm" responsive />
-            <x-mary-button label="Notifications" icon="o-bell" link="###" class="btn-ghost btn-sm" responsive />
-        </x-slot:actions>
+
+        <div>
+
+            <x-slot:actions>
+                {{-- <x-mary-button label="View Service" class="btn bg-blue-custom  text-white border-0" /> --}}
+                {{-- <x-mary-button icon="o-user" label="View User"
+                    class="btn bg-blue-custom  text-white border-0 btn-small" /> --}}
+
+                <x-mary-dropdown class="text-sm h-8 w-12">
+                    {{-- <x-mary-menu-item title="Archive" icon="o-archive-box" /> --}}
+                    <x-mary-menu-item title="Profile" icon="o-user" />
+                    <x-mary-menu-item title="Log-Out" icon="o-arrow-left-end-on-rectangle" />
+                </x-mary-dropdown>
+                {{-- <x-mary-button label="Messages" icon="o-envelope" link="###"
+                    class="btn bg-blue-custom text-white btn-sm" responsive />
+                <x-mary-button label="Notifications" icon="o-bell" link="###"
+                    class="btn bg-blue-custom text-white btn-sm" responsive /> --}}
+            </x-slot:actions>
     </x-mary-nav>
 
     {{-- The main content with `full-width` --}}
@@ -56,18 +83,21 @@
                             </x-mary-list-item>
                         @endif
                         @if ($currentRoute === 'medicines')
-                            <x-mary-menu-item title="Medicines" icon="o-home" link="{{ route('dashboard') }}" />
-                            <x-mary-menu-item title="Inventory" icon="o-document" link="{{ route('medicines') }}" />
-                            <x-mary-menu-item title="Track History" icon="o-document" link="{{ route('patients') }}" />
-                            <x-mary-menu-item title="Activity Logs" icon="o-document" />
-                            <x-mary-menu-item title="Citizen Info" icon="o-document" />
-                            <x-mary-menu-item title="Transactions" icon="o-document" />
-                            <x-mary-menu-item title="User Management" icon="o-document" />
-                            <x-mary-menu-sub title="Settings" icon="o-cog-6-tooth">
-                                <x-mary-menu-item title="Profile" icon="o-user" />
-                                <x-mary-menu-item title="Inbox" icon="o-archive-box" link="####" />
-                                <x-mary-menu-item title="Accessibility" icon="o-information-circle" link="####" />
-                            </x-mary-menu-sub>
+                            <div class="rounded-lg bg-white p-4">
+                                <x-mary-menu-item title="Medicines" icon="o-home" link="{{ route('dashboard') }}" />
+                                <x-mary-menu-item title="Inventory" icon="o-document" link="{{ route('medicines') }}" />
+                                <x-mary-menu-item title="Track History" icon="o-document" link="{{ route('patients') }}" />
+                                <x-mary-menu-item title="Activity Logs" icon="o-document" />
+                                <x-mary-menu-item title="Citizen Info" icon="o-document" />
+                                <x-mary-menu-item title="Transactions" icon="o-document" />
+                                <x-mary-menu-item title="User Management" icon="o-document" />
+                                <x-mary-menu-sub title="Settings" icon="o-cog-6-tooth">
+                                    <x-mary-menu-item title="Profile" icon="o-user" />
+                                    <x-mary-menu-item title="Inbox" icon="o-archive-box" link="####" />
+                                    <x-mary-menu-item title="Accessibility" icon="o-information-circle" link="####" />
+                                </x-mary-menu-sub>
+                            </div>
+
                         @elseif($currentRoute === 'patients')
                             <x-mary-menu-item title="Patient Table" icon="o-home" link="{{ route('dashboard') }}" />
                             <x-mary-menu-item title="Diagnostic" icon="o-document" link="{{ route('medicines') }}" />
@@ -88,10 +118,30 @@
             </x-slot:content>
         </div>
     </x-mary-main>
-
-    {{-- TOAST area --}}
+    <footer class="footer sm:footer-horizontal bg-[#e5e5e5] text-neutral-content items-center p-2 px-4">
+        <aside class="grid-flow-col items-center">
+            <div class="avatar">
+                <div class="w-10 p rounded-full shadow-lg mr-3">
+                    <label class="swap swap-rotate">
+                        <input type="checkbox" />
+                        <div class="swap-on">
+                            <img src="images/taguig-logo.svg" alt="On State" />
+                        </div>
+                        <div class="swap-off">
+                            <img src="images/taguig-logo.svg" alt="Off State" />
+                        </div>
+                    </label>
+                </div>
+            </div>
+            <p class="text-black font-regular">Copyright © 2025 - All right reserved</p>
+        </aside>
+        <nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+            <p class="text-black">
+                <b>Need any help?</b> Call this number +639673411161
+            </p>
+        </nav>
+    </footer>
     <x-mary-toast />
-
 </body>
 @livewireScripts
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.app>
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-        <x-mary-header title="Dashboard" icon="o-beaker" icon-classes="bg-error text-white rounded-full  w-7 h-7"
+        <x-mary-header title="Dashboard" icon="o-beaker" icon-classes="bg-red-custom text-white rounded-full  w-7 h-7"
             style="margin-bottom:0%" separator />
 
         @php
@@ -28,88 +28,141 @@
                     'description' => 'Where burnout is just a fancy term for Tuesday.',
                 ],
             ];
-
         @endphp
 
-        <x-mary-carousel :slides="$slides" class="!h-72" />
+        <x-mary-carousel :slides="$slides" autoplay class="!h-72" />
         <div class="flex flex-col md:flex-row gap-5">
             <div class="bg-base-300 p-5 rounded-lg w-full md:w-90 flex flex-col gap-5">
-                <x-mary-stat title="Messages" value="44" icon="o-envelope" tooltip="Hello" color="text-primary" />
+                <x-mary-stat title="Messages" value="44" icon="o-envelope" tooltip="Hello" color="text-red-custom" />
 
                 <x-mary-stat title="Sales" description="This month" value="22.124" icon="o-arrow-trending-up"
-                    tooltip-bottom="There" />
+                    tooltip-bottom="There" color="text-red-custom" />
 
                 <x-mary-stat title="Lost" description="This month" value="34" icon="o-arrow-trending-down"
-                    tooltip-left="Ops!" />
+                    tooltip-left="Ops!" color="text-red-custom" />
 
                 <x-mary-stat title="Sales" description="This month" value="22.124" icon="o-arrow-trending-down"
-                    class="text-orange-500" color="text-pink-500" tooltip-right="Gosh!" />
+                    class="text-orange-500" color="text-red-custom" tooltip-right="Gosh!" />
             </div>
 
-            <div class="bg-base-200  flex my-2 p-6 rounded-lg" x-data="{loading:true}">
+            <div class="bg-base-300 flex p-6 rounded-lg" x-data="{loading:true}">
                 <div class="grid grid-cols-1 md:grid-cols-[3fr_3fr]  my-4 gap-6">
-                    <div class="card bg-base-100 h-100 shadow-sm">
-                        <figure>
-                            <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                                alt="Shoes" />
-                        </figure>
-                        <div class="p-6">
-                            <h2 class="card-title">
-                                Card Title
-                                <div class="badge badge-secondary">NEW</div>
-                            </h2>
-                            <p>A card component has a figure, a body part, and inside body there are title and actions
-                                parts
-                            </p>
-                            <div class="card-actions justify-end">
-                                <div class="badge badge-outline">Fashion</div>
-                                <div class="badge badge-outline">Products</div>
+                    <div class="relative rounded-lg shadow-md hover:shadow-lg overflow-hidden duration-300">
+                        <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" class="w-full h-full object-cover" />
+
+                        <div class="absolute inset-0 bg-black/50   z-25"></div>
+
+                        <div
+                            class="absolute inset-0 flex flex-col justify-between hover:bg-black/50 duration-300 p-6 z-50">
+                            <div>
+                                <div class="flex items-center space-x-2">
+                                    <x-hero <h2 class="text-white text-xl font-semibold">Medicine Tracker</h2>
+                                        <span
+                                            class="bg-blue-custom text-white text-xs font-bold px-2 py-1 rounded">NEW</span>
+                                </div>
+                                <p class="text-white text-md mt-2">
+                                    A card component has a figure, a body part, and inside body there are title and
+                                    actions parts.
+                                </p>
+                            </div>
+
+                            <div class="mt-4">
+                                {{-- <div class="flex gap-6 mb-2">
+                                    <div class="badge bg-blue-custom border-0 text-white p-3">Secondary</div>
+                                    <div class="badge bg-blue-custom border-0 text-white p-3">Secondary</div>
+                                </div>
+                                <div class="border-t border-white border-opacity-50 my-2"></div> --}}
+                                <div class="flex justify-end">
+                                    <a href="{{ route('medicines') }}"
+                                        class="bg-red-600 hover:bg-red-700 text-white text-sm font-medium px-4 py-2 rounded transition"
+                                        wire:navigate>
+                                        View Medicines
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="relative rounded-lg shadow-md hover:shadow-lg overflow-hidden duration-300">
+                        <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" class="w-full h-full object-cover" />
+
+                        <div class="absolute inset-0 bg-black/50 z-25"></div>
+
+                        <div
+                            class="absolute inset-0 flex flex-col justify-between hover:bg-black/50 duration-300 p-6 z-50">
+                            <div>
+                                <div class="flex items-center space-x-2">
+                                    <h2 class="text-white text-xl font-semibold">Patient Checker</h2>
+                                    <span
+                                        class="bg-blue-custom text-white text-xs font-bold px-2 py-1 rounded">NEW</span>
+                                </div>
+                                <p class="text-white text-md mt-2">
+                                    A card component has a figure, a body part, and inside body there are title and
+                                    actions parts.
+                                </p>
+                            </div>
+
+                            <div class="mt-4">
+                                {{-- <div class="flex gap-6 mb-2">
+                                    <div class="badge badge-soft bg-blue-custom border-0 text-white p-3">Secondary</div>
+                                    <div class="badge badge-soft bg-blue-custom border-0 text-white p-3">Secondary</div>
+                                </div>
+                                <div class="border-t border-white border-opacity-50 my-2"></div> --}}
+                                <div class="flex justify-end">
+                                    <a href="{{ route('patients') }}"
+                                        class="bg-red-600 hover:bg-red-700 text-white text-sm font-medium px-4 py-2 rounded transition"
+                                        wire:navigate>
+                                        View Patients
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="stats bg-base-100 border-base-300 border">
+                        <div class="stat">
+                            <div class="stat-title">Overview of Medicine</div>
+                            <div class="stat-value">$89,400</div>
+                            <div class="stat-actions">
+                                <button class="btn btn-xs btn-success">Add funds</button>
                             </div>
                         </div>
 
-                        <div class="card-body mt-2">
-
+                        <div class="stat">
+                            <div class="stat-title">Overview of Patients</div>
+                            <div class="stat-value">$89,400</div>
+                            <div class="stat-actions">
+                                <button class="btn btn-xs">Withdrawal</button>
+                                <button class="btn btn-xs">Deposit</button>
+                            </div>
                         </div>
                     </div>
 
-                    <div class="card bg-base-100 h-100 shadow-sm">
-                        <figure>
-                            <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                                alt="Shoes" />
-                        </figure>
-                        <div class="p-6">
-                            <h2 class="card-title">
-                                Card Title
-                                <div class="badge badge-secondary">NEW</div>
-                            </h2>
-                            <p>A card component has a figure, a body part, and inside body there are title and actions
-                                parts
-                            </p>
-                            <div class="card-actions justify-end">
-                                <div class="badge badge-outline">Fashion</div>
-                                <div class="badge badge-outline">Products</div>
+                    <div class="stats bg-base-100 border-base-300 border">
+                        <div class="stat">
+                            <div class="stat-title">Overview of Medicine</div>
+                            <div class="stat-value">$89,400</div>
+                            <div class="stat-actions">
+                                <button class="btn btn-xs btn-success">Add funds</button>
                             </div>
                         </div>
 
-                        <div class="card-body mt-2 flex justify-end">
+                        <div class="stat">
+                            <div class="stat-title">Overview of Patients</div>
+                            <div class="stat-value">$89,400</div>
+                            <div class="stat-actions">
+                                <button class="btn btn-xs">Withdrawal</button>
+                                <button class="btn btn-xs">Deposit</button>
+                            </div>
                         </div>
                     </div>
-                            <x-mary-button label="View Service" class="btn-primary " />
-
-                        </div>
-                    </div>
-
                 </div>
             </div>
         </div>
-
-
-
-
-
-
-
-        <!-- @livewire(name: 'chart') -->
+        {{-- @livewire(name: 'chart') --}}''
         <!-- <div class="join flex justify-end my-8">
             <div>
                 <div>
